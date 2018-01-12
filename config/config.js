ac91f00ff9bf9f1223072d31183b140742b0d7de
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-12 08:46:13 -0800

    API config file

---
+++ config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    URL: 'http://localhost/ica',
+    URL_LOGIN: 'http://localhost/login',
+    LATITUDE_INITIAL: '39.3781',    // Center of Spain
+    LONGITUDE_INITIAL: '-3.6795366',
+    LATLNG: '36.1440, -8.411540, 43.527061, 3.1934',    // map bounds in form lat1, lng1, lat2, lng2
+    MAPS_KEY: 'AIzaSyBsep3bl674WDUjos56zB9qJVMEYoRqbf0',
+    AQICN_KEY: 'e9f9eb824b65fdc5a494c0d4eedb10669c559350',
+    GOOGLEMAPS_CONFIG: {
+        key: 'AIzaSyBsep3bl674WDUjos56zB9qJVMEYoRqbf0',
+        stagger_time: 1000, // for elevationPath
+        encode_polylines: false,
+        secure: true,   // use https
+    }
+}
